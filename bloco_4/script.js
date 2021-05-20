4f35aaa9236eebff4f988a5ed8b0630692616478
Leonardo Martins <leonardomartins07@gmail.com>
2021-05-20 16:10:06 -0300

    4.1 comentado

--- bloco_4/script.js
+++ bloco_4/script.js
@@ -134,42 +134,42 @@ let c = 98;
 // else
 //   console.log(lucro)
 
-let salarioBruto=6000
-let INSS=0
-let IR=0
+// let salarioBruto=6000
+// let INSS=0
+// let IR=0
 
-if (salarioBruto<=1556.94 & salarioBruto>0){
-  INSS=salarioBruto*0.08
-}
-else if (salarioBruto>=1556.95 &salarioBruto<=2594.92){
-  INSS=salarioBruto*0.09
-}
-else if (salarioBruto>=2594.93 &salarioBruto<=5189.82){
-  INSS=salarioBruto*0.11
-}
-else if (salarioBruto>5189.82){
-  INSS=570.88
-}
-else {
-  console.log('mas ta pagando pra trabalhar?')
-}
+// if (salarioBruto<=1556.94 & salarioBruto>0){
+//   INSS=salarioBruto*0.08
+// }
+// else if (salarioBruto>=1556.95 &salarioBruto<=2594.92){
+//   INSS=salarioBruto*0.09
+// }
+// else if (salarioBruto>=2594.93 &salarioBruto<=5189.82){
+//   INSS=salarioBruto*0.11
+// }
+// else if (salarioBruto>5189.82){
+//   INSS=570.88
+// }
+// else {
+//   console.log('mas ta pagando pra trabalhar?')
+// }
 
-let salarioBase=salarioBruto-INSS
+// let salarioBase=salarioBruto-INSS
 
-if (salarioBase<=1903.98){
-  IR=0 
-}
-else if(salarioBase>=1903.99 &salarioBase<=2826.65){
-  IR=salarioBase*0.075-142.80
-}
-else if(salarioBase>=2826.66 &salarioBase<=3751.05){
-  IR=salarioBase*0.15-354.80
-}
-else if(salarioBase>=3751.06 &salarioBase<=4664.68){
-  IR=salarioBase*0.225-636.13
-}
-else if (salarioBase>4664.68){
-  IR=salarioBase*0.275-869.36
-}
-let salarioLiquid=salarioBase-IR
-console.log(salarioLiquid)
+// if (salarioBase<=1903.98){
+//   IR=0 
+// }
+// else if(salarioBase>=1903.99 &salarioBase<=2826.65){
+//   IR=salarioBase*0.075-142.80
+// }
+// else if(salarioBase>=2826.66 &salarioBase<=3751.05){
+//   IR=salarioBase*0.15-354.80
+// }
+// else if(salarioBase>=3751.06 &salarioBase<=4664.68){
+//   IR=salarioBase*0.225-636.13
+// }
+// else if (salarioBase>4664.68){
+//   IR=salarioBase*0.275-869.36
+// }
+// let salarioLiquid=salarioBase-IR
+// console.log(salarioLiquid)
